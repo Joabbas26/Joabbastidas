2047bb949a665c09d5156cf5cf07fca8898dab4a
Joab <joabbastidas@gmail.com>
2022-03-07 13:36:33 -0500

    Responsive landing page

--- src/Home.js
+++ src/Home.js
@@ -3,29 +3,33 @@ import './Home.scss'
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 import { faAmazon, faFacebook, faGithub, faInstagram, faTwitter } from "@fortawesome/free-brands-svg-icons"
 
+
 export default function Home() {
   return (
     <div className='HomeDiv'>
-          <div className='grayTextDiv'>
-            <p className='homeTitle'>Welcome to My Site</p>
-            <div className='textsDiv'>
-              <p className='devText'>[ HTML ]</p>
-              <p className='artText'>Artist</p>
-              <p className= 'gamesText'>Games</p>
-              <p className='mangaText'> 漫画 </p>
-              <p className='mangaSubText'>(Man-ga)</p>
-              <p className='novelText'>Novelist</p>
-            </div>
-            <div className='iconDiv'>
-                <FontAwesomeIcon icon={faAmazon}/>
-                <FontAwesomeIcon icon={faGithub}/>
-                <FontAwesomeIcon icon={faInstagram}/>
-                <FontAwesomeIcon icon={faTwitter} />
-                <FontAwesomeIcon icon={faFacebook} /> 
+      <p className='homeTitle'>Welcome to My Site</p>
+          <div className='container' id='grayTextDiv'>
+            <div className='row' id='optionsDiv'>
+              <div className='col-md-4 col-sm-12' id='circleDiv'>
+                <img className='img-fluid' id='circleImage' src={"https://file.rendit.io/n/AiJon90Vza9fDzbo36N2.png"} alt="mainImage"/>
+              </div>
+              <div className='col-md-5 col-sm-6' id='textsDiv'>
+                <p className='devText'>[ HTML ]</p>
+                <p className='artText'>Artist</p>
+                <p className= 'gamesText'>Games</p>
+                <p className='mangaText'> 漫画 </p>
+                <p className='mangaSubText'>(Man-ga)</p>
+                <p className='novelText'>Novelist</p>
+              </div>
+              <div className='col-md-3 col-sm-6' id='iconDiv'>
+                  <FontAwesomeIcon icon={faAmazon}/>
+                  <FontAwesomeIcon icon={faGithub}/>
+                  <FontAwesomeIcon icon={faInstagram}/>
+                  <FontAwesomeIcon icon={faTwitter} />
+                  <FontAwesomeIcon icon={faFacebook} /> 
+              </div>
             </div>
           </div>
-          <img className='circleBackground' src={"https://file.rendit.io/n/Bt3oEMNwsyB8p6foShnS.svg"} alt=''/>
-          <img className='circleImage' src={"https://file.rendit.io/n/AiJon90Vza9fDzbo36N2.png"} alt="mainImage"/>
         </div>
   );
 };
