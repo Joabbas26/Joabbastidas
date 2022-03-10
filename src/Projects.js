4242ec07111d7f420f17dff0b86947793a06fa93
Joab <joabbastidas@gmail.com>
2022-03-10 10:56:35 -0500

    Finished projects section

--- src/Projects.js
+++ src/Projects.js
@@ -1,7 +1,35 @@
 import React from 'react'
+import './Projects.scss'
+import { Link } from 'react-router-dom';
+
 
 export default function Projects() {
   return (
-    <div>Projects</div>
+    <div className='projectsDiv'>
+      <p className='projectsTitle'>Projects</p>
+      <p className='webDevTitle'>Full Stack</p>
+      <div className='container' id='webDevSection'>
+        <div className='row'>
+          <div className='col' id='card'>
+          <Link to="/"><img className='img-fluid' id='projectImage' src={"https://media.geeksforgeeks.org/wp-content/uploads/20200405130022/TableCreation.png"} 
+          alt="projectImage"/></Link>
+          <div className="card-body">
+            <h5 className="card-title">Rank Table</h5>
+            <p className="card-text">Open a modal and add data to a table. Perform calculation based on data. Able to delete rows and edit data.</p>
+          </div>
+        </div>
+        <div className='col' id='card'>
+        <Link to="/"><img className='img-fluid' id='projectImage' src={"https://play-lh.googleusercontent.com/zoAQwsds7P2exgVXFUiV8faEmblggMeZ59pILSa9B1cCYwHD7PFExvHhdDZCvW1enpEY"} 
+          alt="projectImage"/></Link>
+          <div className="card-body">
+            <h5 className="card-title">Weather API</h5>
+            <p className="card-text">Make an api call to get local weather data and show in a detailed view while being UI friendly </p>
+          </div>
+        </div>
+        </div>
+        
+      </div>
+      
+    </div>
   )
 }
