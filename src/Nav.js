d29a87e8be8e585e89c70d78d1e3ac1e4107b3a7
Joab <joabbastidas@gmail.com>
2022-03-12 15:16:51 -0500

    Finished hamburger menu and bug fixes

--- src/Nav.js
+++ src/Nav.js
@@ -1,21 +1,47 @@
-import React from 'react';
+import React, {useState, useEffect} from 'react';
 import './Nav.scss';
 import { Link, NavLink} from 'react-router-dom';
 
+
 export default function Nav() {
+
+    const [toggleMenu, setToggleMenu] = useState(false);
+    const [navStyle, setNavStyle] = useState();
+    const toggleNav = () => {
+        setToggleMenu(!toggleMenu);
+        toggleMenu === true ? setNavStyle({display:'none'}) : setNavStyle({display:'block'});
+      }
+
+      const [screenWidth, setScreenWidth] = useState(window.innerWidth)
+
+      useEffect(() => {
+
+        const changeWidth = () => {
+          setScreenWidth(window.innerWidth);
+        }
+        window.addEventListener('resize', changeWidth)
+        return () => {
+            window.removeEventListener('resize', changeWidth)
+        }
+    
+    
+      }, [])
+
   return (
-    <div className='NavDiv'>
+    <div className='navDiv'>
         <div className="nav-container">
             <NavLink to="/" className="brand">Joab Bastidas</NavLink>
             <nav>
                 <div className="nav-mobile">
-                    <button id="nav-toggle" href="#!"><span></span></button>
+                    <button id="nav-toggle" onClick={toggleNav}><span></span></button>
                 </div>
-                <ul className="nav-list">
-                    <li><Link to="/projects">Projects</Link></li>
-                    <li><Link to="/about">About</Link></li>
-                    <li><Link to="/contact">Contact</Link></li>
+                {(toggleMenu || screenWidth > 500) && (
+                <ul className="nav-list" style={navStyle}>
+                    <li onClick={toggleNav}><Link to="/projects">Projects</Link></li>
+                    <li onClick={toggleNav}><Link to="/about">About</Link></li>
+                    <li onClick={toggleNav}><Link to="/contact">Contact</Link></li>
                 </ul>
+                )}
             </nav>
         </div>
     </div>
