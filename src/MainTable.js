9b2cec79ba38da13a23bb764739c13f93dcbdb4f
Joab <joabbastidas@gmail.com>
2022-03-16 14:54:28 -0400

    Condensed main table component

--- src/MainTable.js
+++ src/MainTable.js
@@ -1,13 +1,17 @@
 import React, { useState, useRef } from 'react';
-import { Trash, PencilSquare, PlusSquare } from 'react-bootstrap-icons';
-import './MainTable.scss';
+import Button from 'react-bootstrap/Button';
+import Form from 'react-bootstrap/Form';
+import Col from 'react-bootstrap/Col';
+import Row from 'react-bootstrap/Row';
+import Modal from 'react-bootstrap/Modal';
 import { useDispatch, useSelector } from 'react-redux';
 import { toggle } from './reducers/ModalSlice';
+import { CloseButton } from 'react-bootstrap';
+import { Trash, PencilSquare, PlusSquare } from 'react-bootstrap-icons';
+import './MainTable.scss';
 import { toggleEdit } from './reducers/EditModalSlice';
-import { deleteRow, saveRow } from './reducers/NewRowSlice';
+import { addRow, deleteRow, saveRow } from './reducers/NewRowSlice';
 import { v4 as uuidv4 } from 'uuid';
-import MainModal from './MainModal';
-import EditModal from './EditModal';
 
 export default function MainTable() {
 
@@ -26,14 +30,14 @@ export default function MainTable() {
     const [fullTime, setFullTime] = useState('');
     const [recommendation, setRecommendation] = useState('');
 
-    // Handles opening modal
-    const openModalHandler = () => {
-        dispatch(toggle());
-    }
-
      // Used to toggle modal show and hide
      const editModalHandler = () => {
         dispatch(toggleEdit());
+    }
+
+    // Used to toggle modal show and hide
+    const modalHandler = () => {
+        dispatch(toggle());
     }
 
     // Handles deleting row
@@ -43,42 +47,76 @@ export default function MainTable() {
         dispatch(deleteRow( {rowNum: rowIndex} ));  
     }
 
-    const totalRef = useRef(0);
+   
+   const totalRef = useRef(0);
 
-    const getRowTotal = () =>{
-        var calcTotal = 0;
-             // If comp time more than 50 get 30 points 
-             if (companyTime <= 10) {
-                 calcTotal += 10;
-             } else if (companyTime > 10 && companyTime < 50) {
-                 calcTotal += 20;
-             }else if (companyTime >= 50){
-                 calcTotal += 30;
-                 
-             }
-     
-             // If full time get 20 points 
-             if (fullTime === 'Yes') {
-                 calcTotal += 20;
-             } else {
-                 calcTotal += 10;
-             }
-     
-             // If over time more than 5 get 30 points 
-             if (overTime < 5) {
-                 calcTotal += 10;
-             } else {
-                 calcTotal += 20;
-             }
-     
-             // Divide recommendation score by 10 and multiply  by 3 to get 30 points
-             if (recommendation <= 100) {
-                 calcTotal += Math.floor((recommendation / 10) * 3);
-             } else {
-                 calcTotal += 0;
-             }
-            // Sets ref to calculated total score
-            totalRef.current = calcTotal;
+   const getRowTotal = () =>{
+       var calcTotal = 0;
+            // If comp time more than 50 get 30 points 
+            if (companyTime <= 10) {
+                calcTotal += 10;
+            } else if (companyTime > 10 && companyTime < 50) {
+                calcTotal += 20;
+            }else if (companyTime >= 50){
+                calcTotal += 30;
+                
+            }
+    
+            // If full time get 20 points 
+            if (fullTime === 'Yes') {
+                calcTotal += 20;
+            } else {
+                calcTotal += 10;
+            }
+    
+            // If over time more than 5 get 30 points 
+            if (overTime < 5) {
+                calcTotal += 10;
+            } else {
+                calcTotal += 20;
+            }
+    
+            // Divide recommendation score by 10 and multiply  by 3 to get 30 points
+            if (recommendation <= 100) {
+                calcTotal += Math.floor((recommendation / 10) * 3);
+            } else {
+                calcTotal += 0;
+            }
+           // Sets ref to calculated total score
+           totalRef.current = calcTotal;
+  }
+
+  const handleEditSubmit = () => {
+    getRowTotal();
+    // Adds input data to row
+    dispatch(saveRow({
+        //rowNum: rowNumber,
+        fName : firstName,
+        lName : lastName, 
+        compTime : companyTime, 
+        fTime : fullTime,
+        oTime : overTime,
+        recomm: recommendation,
+        total : totalRef.current,
+    }));  
+}
+
+   // Handles adding user data to table
+   const handleSubmit = (e) => {
+       e.preventDefault();
+       dispatch(toggle());
+       getRowTotal();
+   // Adds input data to row
+       dispatch(addRow({
+           //rowNum: rowNumber,
+           fName : firstName,
+           lName : lastName, 
+           compTime : companyTime, 
+           fTime : fullTime,
+           oTime : overTime,
+           recomm: recommendation,
+           total : totalRef.current,
+       }));  
    }
 
     // Handles edit of table row
@@ -116,8 +154,122 @@ export default function MainTable() {
 
     return (
         <div className='mainTable'>
-            <MainModal onHide={openModalHandler} show={isOpen}/>
-            <EditModal onHide={editModalHandler} show={editIsOpen}/>
+            <Modal className='editModal' onHide={editModalHandler} show={editIsOpen}>
+        <Modal.Header>
+            <Modal.Title>Employee Evaluation</Modal.Title>
+            <CloseButton onClick={editModalHandler}/>
+        </Modal.Header>
+
+        <Modal.Body>
+            <Form onSubmit={handleEditSubmit} action="../../post" method="post">
+                <Row className="mb-3">
+                    <Form.Group as={Col} controlId="formGridFirstName">
+                    <Form.Label>First Name</Form.Label>
+                    <Form.Control type="text" placeholder="First Name" 
+                    value={firstName} onInput={e => setFirstName(e.target.value)}/>
+                    </Form.Group>
+
+                    <Form.Group as={Col} controlId="formGridLastName">
+                    <Form.Label>Last Name</Form.Label>
+                    <Form.Control type="text" placeholder="Last Name" 
+                    value={lastName} onInput={e => setLastName(e.target.value)}/>
+                    </Form.Group>
+                </Row>
+
+                <Form.Group className="mb-3" controlId="formGridYearsAtCompany">
+                    <Form.Label>Time At Company</Form.Label>
+                    <Form.Control placeholder="In Years" type="number" 
+                    value={companyTime} onInput={e => setCompanyTime(e.target.value)}/>
+                </Form.Group>
+
+                <Row className="mb-3">
+                    <Form.Group as={Col} controlId="formGridFullTime">
+                    <Form.Label>Full-Time</Form.Label>
+                    <Form.Control as="select" value={fullTime} onChange={e => setFullTime(e.target.value)}>
+                        <option value='...'>...</option>
+                        <option value='Yes'>Yes</option>
+                        <option value='No'>No</option>
+                    </Form.Control>
+                    </Form.Group>
+
+                    <Form.Group as={Col} controlId="formGridOvertime">
+                    <Form.Label>Overtime</Form.Label>
+                    <Form.Control placeholder='In Hours' type="number" 
+                    value={overTime} onInput={e => setOverTime(e.target.value)}/>
+                    </Form.Group>
+
+                    <Form.Group as={Col} controlId="formGridRecommendtion">
+                    <Form.Label>Recommendation</Form.Label>
+                    <Form.Control placeholder='Score' type="number"
+                    value={recommendation} onInput={e => setRecommendation(e.target.value)}/>
+                    </Form.Group>
+                </Row>
+
+            </Form>
+        </Modal.Body>
+
+        <Modal.Footer>
+            <Button variant="primary" type="submit" onClick={handleSubmit}>Save</Button>
+        </Modal.Footer>
+    </Modal>
+            <Modal className='modal' onHide={modalHandler} show={isOpen}>
+                <Modal.Header>
+                    <Modal.Title>Employee Evaluation</Modal.Title>
+                    <CloseButton onClick={modalHandler}/>
+                </Modal.Header>
+
+                <Modal.Body>
+                    <Form onSubmit={handleSubmit} action="../../post" method="post">
+                        <Row className="mb-3">
+                            <Form.Group as={Col} controlId="formGridFirstName">
+                            <Form.Label>First Name</Form.Label>
+                            <Form.Control type="text" placeholder="First Name" 
+                            value={firstName} onInput={e => setFirstName(e.target.value)}/>
+                            </Form.Group>
+
+                            <Form.Group as={Col} controlId="formGridLastName">
+                            <Form.Label>Last Name</Form.Label>
+                            <Form.Control type="text" placeholder="Last Name" 
+                            value={lastName} onInput={e => setLastName(e.target.value)}/>
+                            </Form.Group>
+                        </Row>
+
+                        <Form.Group className="mb-3" controlId="formGridYearsAtCompany">
+                            <Form.Label>Time At Company</Form.Label>
+                            <Form.Control placeholder="In Years" type="number" 
+                            value={companyTime} onInput={e => setCompanyTime(e.target.value)}/>
+                        </Form.Group>
+
+                        <Row className="mb-3">
+                            <Form.Group as={Col} controlId="formGridFullTime">
+                            <Form.Label>Full-Time</Form.Label>
+                            <Form.Control as="select" value={fullTime} onChange={e => setFullTime(e.target.value)}>
+                                <option value='...'>...</option>
+                                <option value='Yes'>Yes</option>
+                                <option value='No'>No</option>
+                            </Form.Control>
+                            </Form.Group>
+
+                            <Form.Group as={Col} controlId="formGridOvertime">
+                            <Form.Label>Overtime</Form.Label>
+                            <Form.Control placeholder='In Hours' type="number" 
+                            value={overTime} onInput={e => setOverTime(e.target.value)}/>
+                            </Form.Group>
+
+                            <Form.Group as={Col} controlId="formGridRecommendtion">
+                            <Form.Label>Recommendation</Form.Label>
+                            <Form.Control placeholder='Score' type="number"
+                            value={recommendation} onInput={e => setRecommendation(e.target.value)}/>
+                            </Form.Group>
+                        </Row>
+
+                    </Form>
+                </Modal.Body>
+
+                <Modal.Footer>
+                    <Button variant="primary" type="submit" onClick={handleSubmit}>Save</Button>
+                </Modal.Footer>
+            </Modal>
             <h1 className='tableTitle'>Employee Ranking</h1>
             <div className="table-responsive">
                     <table className="table table-bordered">
@@ -152,7 +304,7 @@ export default function MainTable() {
                                 ))}
                             </tbody>
                     </table>
-                    <PlusSquare className='addRow' id='icon' onClick={openModalHandler} style={{height:30, width:30}}/>
+                    <PlusSquare className='addRow' id='icon' onClick={modalHandler} style={{height:30, width:30}}/>
                 </div>
             </div>
     )
