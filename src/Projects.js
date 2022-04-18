43380d43e203802d89e90c0d9d8a7591fff207c6
Joab <joabbastidas@gmail.com>
2022-04-18 11:47:53 -0400

    Comming soon for future projects

--- src/Projects.js
+++ src/Projects.js
@@ -34,22 +34,7 @@ export default function Projects() {
       <p className='projectTitle' id='artSection'>Art</p>
       <div className='container' id='projectsSection'>
         <div className='row'>
-          <div className='col' id='card'>
-          <Link to="/mainTable"><img className='img-fluid' id='projectImage' src={"https://media.geeksforgeeks.org/wp-content/uploads/20200405130022/TableCreation.png"} 
-          alt="projectImage"/></Link>
-          <div className="card-body">
-            <h5 className="card-title">Rank Table</h5>
-            <p className="card-text">Open a modal and add data to a table. Perform calculation based on data. Able to delete rows and edit data.</p>
-          </div>
-        </div>
-        <div className='col' id='card'>
-        <Link to="/weather"><img className='img-fluid' id='projectImage' src={"https://images-na.ssl-images-amazon.com/images/I/81K-NehZ++L.png"} 
-          alt="projectImage"/></Link>
-          <div className="card-body">
-            <h5 className="card-title">Weather API</h5>
-            <p className="card-text">Make an api call to get local weather data and show in a detailed view while being UI friendly </p>
-          </div>
-        </div>
+          Comming Soon!
         </div>
       </div>
 
@@ -57,22 +42,7 @@ export default function Projects() {
       <p className='projectTitle' id='gamesSection'>Games</p>
       <div className='container' id='projectsSection'>
         <div className='row'>
-          <div className='col' id='card'>
-          <Link to="/mainTable"><img className='img-fluid' id='projectImage' src={"https://media.geeksforgeeks.org/wp-content/uploads/20200405130022/TableCreation.png"} 
-          alt="projectImage"/></Link>
-          <div className="card-body">
-            <h5 className="card-title">Rank Table</h5>
-            <p className="card-text">Open a modal and add data to a table. Perform calculation based on data. Able to delete rows and edit data.</p>
-          </div>
-        </div>
-        <div className='col' id='card'>
-        <Link to="/weather"><img className='img-fluid' id='projectImage' src={"https://images-na.ssl-images-amazon.com/images/I/81K-NehZ++L.png"} 
-          alt="projectImage"/></Link>
-          <div className="card-body">
-            <h5 className="card-title">Weather API</h5>
-            <p className="card-text">Make an api call to get local weather data and show in a detailed view while being UI friendly </p>
-          </div>
-        </div>
+          Comming Soon!
         </div>
       </div>
 
@@ -80,22 +50,7 @@ export default function Projects() {
       <p className='projectTitle' id='mangaSection'>Manga</p>
         <div className='container' id='projectsSection'>
           <div className='row'>
-            <div className='col' id='card'>
-            <Link to="/mainTable"><img className='img-fluid' id='projectImage' src={"https://media.geeksforgeeks.org/wp-content/uploads/20200405130022/TableCreation.png"} 
-            alt="projectImage"/></Link>
-            <div className="card-body">
-              <h5 className="card-title">Rank Table</h5>
-              <p className="card-text">Open a modal and add data to a table. Perform calculation based on data. Able to delete rows and edit data.</p>
-            </div>
-          </div>
-          <div className='col' id='card'>
-          <Link to="/weather"><img className='img-fluid' id='projectImage' src={"https://images-na.ssl-images-amazon.com/images/I/81K-NehZ++L.png"} 
-            alt="projectImage"/></Link>
-            <div className="card-body">
-              <h5 className="card-title">Weather API</h5>
-              <p className="card-text">Make an api call to get local weather data and show in a detailed view while being UI friendly </p>
-            </div>
-          </div>
+            Comming Soon!
           </div>
         </div>
 
@@ -103,22 +58,7 @@ export default function Projects() {
       <p className='projectTitle' id='booksSection'>Books</p>
       <div className='container' id='projectsSection'>
         <div className='row'>
-          <div className='col' id='card'>
-          <Link to="/mainTable"><img className='img-fluid' id='projectImage' src={"https://media.geeksforgeeks.org/wp-content/uploads/20200405130022/TableCreation.png"} 
-          alt="projectImage"/></Link>
-          <div className="card-body">
-            <h5 className="card-title">Rank Table</h5>
-            <p className="card-text">Open a modal and add data to a table. Perform calculation based on data. Able to delete rows and edit data.</p>
-          </div>
-        </div>
-        <div className='col' id='card'>
-        <Link to="/weather"><img className='img-fluid' id='projectImage' src={"https://images-na.ssl-images-amazon.com/images/I/81K-NehZ++L.png"} 
-          alt="projectImage"/></Link>
-          <div className="card-body">
-            <h5 className="card-title">Weather API</h5>
-            <p className="card-text">Make an api call to get local weather data and show in a detailed view while being UI friendly </p>
-          </div>
-        </div>
+          Comming Soon!
         </div>
       </div>
     </div>
