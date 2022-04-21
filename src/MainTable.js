ddb5546fb760e66124014f2d653b015c1537d2e7
Joab <joabbastidas@gmail.com>
2022-04-21 16:23:50 -0400

    Started validation with formilk

--- src/MainTable.js
+++ src/MainTable.js
@@ -104,9 +104,14 @@ export default function MainTable() {
    // Handles adding user data to table
    const handleSubmit = (e) => {
        e.preventDefault();
-       dispatch(toggle());
-       getRowTotal();
-   // Adds input data to row
+       if(firstName === ''){
+        // Switch with proper for validator
+        alert('Cannot leave field empty')
+       }
+       else{
+        dispatch(toggle());
+        getRowTotal();
+        // Adds input data to row
        dispatch(addRow({
            //rowNum: rowNumber,
            fName : firstName,
@@ -117,6 +122,8 @@ export default function MainTable() {
            recomm: recommendation,
            total : totalRef.current,
        }));  
+       }
+       
    }
 
     // Handles edit of table row
@@ -229,8 +236,8 @@ export default function MainTable() {
                         <Row className="mb-3">
                             <Form.Group as={Col} controlId="formGridFirstName">
                             <Form.Label>First Name</Form.Label>
-                            <Form.Control type="text" placeholder="First Name" 
-                            value={firstName} onInput={e => setFirstName(e.target.value)}/>
+                            <Form.Control type="text" placeholder="First Name"
+                            value={firstName} onInput={e => setFirstName(e.target.value)} />
                             </Form.Group>
 
                             <Form.Group as={Col} controlId="formGridLastName">
@@ -319,5 +326,151 @@ export default function MainTable() {
 
 /*
 
+const { Formik } = formik;
+
+const schema = yup.object().shape({
+  firstName: yup.string().required(),
+  lastName: yup.string().required(),
+  username: yup.string().required(),
+  city: yup.string().required(),
+  state: yup.string().required(),
+  zip: yup.string().required(),
+  terms: yup.bool().required().oneOf([true], 'Terms must be accepted'),
+});
+
+function FormExample() {
+  return (
+    <Formik
+      validationSchema={schema}
+      onSubmit={console.log}
+      initialValues={{
+        firstName: 'Mark',
+        lastName: 'Otto',
+        username: '',
+        city: '',
+        state: '',
+        zip: '',
+        terms: false,
+      }}
+    >
+      {({
+        handleSubmit,
+        handleChange,
+        handleBlur,
+        values,
+        touched,
+        isValid,
+        errors,
+      }) => (
+        <Form noValidate onSubmit={handleSubmit}>
+          <Row className="mb-3">
+            <Form.Group as={Col} md="4" controlId="validationFormik01">
+              <Form.Label>First name</Form.Label>
+              <Form.Control
+                type="text"
+                name="firstName"
+                value={values.firstName}
+                onChange={handleChange}
+                isValid={touched.firstName && !errors.firstName}
+              />
+              <Form.Control.Feedback>Looks good!</Form.Control.Feedback>
+            </Form.Group>
+            <Form.Group as={Col} md="4" controlId="validationFormik02">
+              <Form.Label>Last name</Form.Label>
+              <Form.Control
+                type="text"
+                name="lastName"
+                value={values.lastName}
+                onChange={handleChange}
+                isValid={touched.lastName && !errors.lastName}
+              />
+
+              <Form.Control.Feedback>Looks good!</Form.Control.Feedback>
+            </Form.Group>
+            <Form.Group as={Col} md="4" controlId="validationFormikUsername">
+              <Form.Label>Username</Form.Label>
+              <InputGroup hasValidation>
+                <InputGroup.Text id="inputGroupPrepend">@</InputGroup.Text>
+                <Form.Control
+                  type="text"
+                  placeholder="Username"
+                  aria-describedby="inputGroupPrepend"
+                  name="username"
+                  value={values.username}
+                  onChange={handleChange}
+                  isInvalid={!!errors.username}
+                />
+                <Form.Control.Feedback type="invalid">
+                  {errors.username}
+                </Form.Control.Feedback>
+              </InputGroup>
+            </Form.Group>
+          </Row>
+          <Row className="mb-3">
+            <Form.Group as={Col} md="6" controlId="validationFormik03">
+              <Form.Label>City</Form.Label>
+              <Form.Control
+                type="text"
+                placeholder="City"
+                name="city"
+                value={values.city}
+                onChange={handleChange}
+                isInvalid={!!errors.city}
+              />
+
+              <Form.Control.Feedback type="invalid">
+                {errors.city}
+              </Form.Control.Feedback>
+            </Form.Group>
+            <Form.Group as={Col} md="3" controlId="validationFormik04">
+              <Form.Label>State</Form.Label>
+              <Form.Control
+                type="text"
+                placeholder="State"
+                name="state"
+                value={values.state}
+                onChange={handleChange}
+                isInvalid={!!errors.state}
+              />
+              <Form.Control.Feedback type="invalid">
+                {errors.state}
+              </Form.Control.Feedback>
+            </Form.Group>
+            <Form.Group as={Col} md="3" controlId="validationFormik05">
+              <Form.Label>Zip</Form.Label>
+              <Form.Control
+                type="text"
+                placeholder="Zip"
+                name="zip"
+                value={values.zip}
+                onChange={handleChange}
+                isInvalid={!!errors.zip}
+              />
+
+              <Form.Control.Feedback type="invalid">
+                {errors.zip}
+              </Form.Control.Feedback>
+            </Form.Group>
+          </Row>
+          <Form.Group className="mb-3">
+            <Form.Check
+              required
+              name="terms"
+              label="Agree to terms and conditions"
+              onChange={handleChange}
+              isInvalid={!!errors.terms}
+              feedback={errors.terms}
+              feedbackType="invalid"
+              id="validationFormik0"
+            />
+          </Form.Group>
+          <Button type="submit">Submit form</Button>
+        </Form>
+      )}
+    </Formik>
+  );
+}
+
+render(<FormExample />);
 
  */
