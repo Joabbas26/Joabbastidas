808f867a4f6a8ee79f628fb1584319f85d3e01e9
Joab <joabbastidas@gmail.com>
2022-04-26 15:10:17 -0400

    Testing browser router

--- src/App.js
+++ src/App.js
@@ -21,14 +21,15 @@ function App() {
         <Nav/>
         {/* Contains website data in multiple routes */}
           <Routes>
-              <Route path='/' element={<Home/>} />
-              <Route path='/projects' element={<Projects/>} /> 
-              <Route path='/about' element={<About/>} />
-              <Route path='/contact' element={<Contact/>} />
-              <Route path='/mainTable' element={<MainTable/>} />
-              <Route path='/weather' element={<WeatherApp/>} />
+                <Route index element={<Home />} />
+                {/* <Route path='/' element={<Home/>}/> */}
+                <Route path='/projects' element={<Projects/>} /> 
+                <Route path='/about' element={<About/>} />
+                <Route path='/contact' element={<Contact/>} />
+                <Route path='/mainTable' element={<MainTable/>} />
+                <Route path='/weather' element={<WeatherApp/>} />
           </Routes >
-        <Footer/>
+          <Footer/>
         </div>
       </BrowserRouter>
     </Provider>
